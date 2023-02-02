693cdf3f1b520439ca4a2eaeb349bf2650771a98
Mateo Besednik <mateo.besednik@gmail.com>
2023-02-02 03:57:14 +0100

    small change

--- js/script.js
+++ js/script.js
@@ -33,9 +33,11 @@ document.addEventListener('click', (e) => {
     navToggler.classList.add('hide');
     if (e.target.classList.contains('nav-item')) {
       toggleNavbar();
+      document.body.classList.toggle('hide-scrolling');
     } else {
       hideSection();
-      document.body.classList.add('hide-scrolling');
+      // document.body.classList.add('hide-scrolling');
+      document.body.classList.toggle('hide-scrolling');
     }
     setTimeout(() => {
       document
